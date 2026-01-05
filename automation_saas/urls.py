eb892e428de65efab9898c459e719c46a78f278d
Yakir Tubul <yakirtubul9@gmail.com>
2026-01-05 21:05:14 +0200

    Add probe endpoint

--- automation_saas/urls.py
+++ automation_saas/urls.py
@@ -5,14 +5,14 @@ from django.contrib.auth import views as auth_views
 
 from core import views as core_views
 
+from django.http import HttpResponse
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # התחברות / התנתקות
+    path("probe/", lambda r: HttpResponse("PROBE_OK")),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-
-    # UI ללקוח (בעל הקליניקה)
     path("", core_views.dashboard, name="dashboard"),
     path("settings/", core_views.settings_view, name="settings"),
 ]
+
